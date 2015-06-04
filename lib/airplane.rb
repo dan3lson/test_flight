50e3f56c23bbdade7375a44aaaa8bb0bbc3afe2a
Danelson Rosa <danelson.rosa@gmail.com>
2015-06-04 19:56:29 -0400

    Completed core and extra credit requirements while also refactoring.

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -1,2 +1,60 @@
 class Airplane
+
+  attr_reader :type, :wing_loading, :horse_power, :fuel
+
+  def initialize(type, wing_loading, horse_power, fuel)
+    @type = type
+    @wing_loading = wing_loading
+    @horse_power = horse_power
+    @status = "off"
+    @flying = false
+    @fuel = fuel
+  end
+
+  def start
+    if status == "off" && fuel >= 25
+      self.status = "on"
+      self.fuel = fuel - 25
+      "airplane started. fuel remaining: #{fuel}"
+    elsif status == "on"
+      "airplane already started"
+    elsif fuel < 25
+      "not enough fuel. walk instead."
+    end
+  end
+
+  def takeoff
+    if status == "off"
+      "airplane not started, please start"
+    else
+      self.flying = true
+      self.fuel = fuel - 35
+      "airplane launched. fuel remaining: #{fuel}"
+    end
+  end
+
+  def land
+    if flying
+      self.fuel = fuel - 50
+      "airplane landed. fuel remaining: #{fuel}"
+    elsif status == "off"
+      "airplane not started, please start"
+    elsif start == "airplane already started"
+      "airplane already on the ground"
+    end
+  end
+
+  def refuel(gas)
+    @fuel += gas
+  end
+
+  def fuel=(value)
+    @fuel = value
+  end
+
+  private
+
+  attr_accessor :status, :flying
+  attr_writer :fuel
+
 end
--- spec/lib/airplane_spec.rb
+++ spec/lib/airplane_spec.rb
@@ -1,16 +1,99 @@
 require_relative "../../lib/airplane"
+require 'pry'
 
 describe Airplane do
-  describe "#initialization" do
-    
-  end
+  let(:airplane) { Airplane.new("cesna", 10, 150, fuel) }
+  let(:fuel) { 100 }
 
-  describe "#land" do
-  end
+  describe "#initialize" do
+    it "has a type" do
+      expect(airplane.type).to eq("cesna")
+    end
 
-  describe "#takeoff" do
+    it "has a wing_loading" do
+      expect(airplane.wing_loading).to eq(10)
+    end
+
+    it "has horse_power" do
+      expect(airplane.horse_power).to eq(150)
+    end
+
+    it "has fuel" do
+      expect(airplane.fuel).to eq(100)
+    end
   end
 
   describe "#start" do
+    context "when there is enough fuel" do
+      let(:fuel) { 100 }
+      context "when the engine is off" do
+        it "returns 'airplane started'" do
+          expect(airplane.start).to eq("airplane started. fuel remaining: 75")
+        end
+      end
+
+      context "when the engine is already on" do
+        it "returns 'airplane already started'" do
+          airplane.start
+          expect(airplane.start).to eq("airplane already started")
+        end
+      end
+    end
+
+    context "when there is not enough fuel" do
+      let(:fuel) { 24 }
+      context "when the engine is off" do
+        it "returns 'not enough fuel. walk instead'" do
+          expect(airplane.start).to eq("not enough fuel. walk instead.")
+        end
+      end
+    end
   end
+
+  describe "#takeoff" do
+    context "when the plane is not started" do
+      it "returns 'airplane not started, please start'" do
+        expect(airplane.takeoff).to eq("airplane not started, please start")
+      end
+    end
+
+    context "when the plane is already started" do
+      it "returns 'airplane launched'" do
+        airplane.start
+        expect(airplane.takeoff).to eq("airplane launched. fuel remaining: 40")
+      end
+    end
+  end
+
+  describe "#land" do
+    context "when the plane isn\'t on" do
+      it "returns 'airplane not started, please start'" do
+        expect(airplane.land).to eq("airplane not started, please start")
+      end
+    end
+
+    context "when the plane is on, but not flying" do
+      it "returns 'airplane already on the ground'" do
+        airplane.start
+        expect(airplane.land).to eq("airplane already on the ground")
+      end
+    end
+
+    context "when the plane is already in the air" do
+      it "returns 'airplane landed'" do
+        airplane.start
+        airplane.takeoff
+        expect(airplane.land).to eq("airplane landed. fuel remaining: -10")
+      end
+    end
+  end
+
+  describe "#fuel" do
+    context "when plane starts" do
+      it "returns 'fuel is now 75'" do
+        airplane.start
+      end
+    end
+  end
+
 end
