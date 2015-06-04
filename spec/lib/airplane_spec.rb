d60f4f195633a667c93df271dd48bf458af6a5a6
Danelson Rosa <danelson.rosa@gmail.com>
2015-06-04 08:38:32 -0400

    Initial repo commit.

---
+++ spec/lib/airplane_spec.rb
@@ -0,0 +1,16 @@
+require_relative "../../lib/airplane"
+
+describe Airplane do
+  describe "#initialization" do
+    
+  end
+
+  describe "#land" do
+  end
+
+  describe "#takeoff" do
+  end
+
+  describe "#start" do
+  end
+end
